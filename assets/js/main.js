26382c109a949c39aa6bf5abc0515289e845ace6
devwulff <p.wulff887@gmail.com>
2022-04-30 21:20:04 +0200

    SlideInMenu added and JS fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
-var mobilemenu = document.querySelector(".mobilemenu");
+let mobilemenu = document.querySelector(".mobilemenu");
+let slideInMenu = document.querySelector('.slidemenu');
+
+// Hamburger Button @ maxWidth: 576px
+
 mobilemenu.onclick = function () {
     this.classList.toggle("checked");
+}
+
+// Slide in Menu
+
+function showMenu() {
+    slideInMenu.classList.toggle('slidemenushow');
+}
+
+function closeMenu() {
+    slideInMenu.style.left = '100%';
+    slideInMenu.style.right = '-100%';
 }
